3f3f358d7be3a65428b83bd32ed551a0204926a9
Sem Schilder <sem@tropical.email>
2017-08-09 18:04:11 +0200

    Correct includes and first config 🔥

--- public/index.php
+++ public/index.php
@@ -3,8 +3,22 @@
 /**
  * Get data for Composer Packages.
  */
-require_once __DIR__ . '../vendor/autoload.php';
-require_once __DIR__ . '../private/loadAppData.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../private/loadAppData.php';
+
+/**
+ * Temporary config for first development:
+ */
+$config = [
+    'unsecure_url' => 'http://flat.dev/',
+    'secure_url'   => 'https://flat.dev/',
+    'database'     => [
+        'user'  => 'user',
+        'password' => 'password',
+        'database' => 'database',
+        'host'     => 'host',
+    ]
+];
 
 $app = new App($config);
 $app->run();
